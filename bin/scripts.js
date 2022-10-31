0b0263c6a07efb9a23fba6592740dc81d8c4b01d
Yuto Watanabe <yuto.w51942@gmail.com>
2022-11-01 01:08:37 +0900

    ビルド方法変更

--- bin/scripts.js
+++ bin/scripts.js
@@ -1,78 +1,138 @@
-/**
- * はてなブログの見出し要素にコピー用のリンクを設置、
- * リンクをクリックすると、その見出しの応じたURLがコピーされる
- */
-define("src/headingLink", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.headingLinkCopy = void 0;
-    const ENTRY_SELECTOR = ".entry-content";
-    const HEADING_SELECTOR = ".entry-content > h1, .entry-content > h2, .entry-content > h3, .entry-content > h4, .entry-content > h5, .entry-content > h6";
-    const HEADING_CLASS_NAME = "heading";
-    function isElement(target) {
-        return target instanceof Element;
+(function () {
+    var defines = {};
+    var entry = [null];
+    function define(name, dependencies, factory) {
+        defines[name] = { dependencies: dependencies, factory: factory };
+        entry[0] = name;
     }
-    class HeadingLinkCopy {
-        headings;
-        constructor() {
-            const headings = this.searchAllHeading();
-            headings.forEach((v) => {
-                this.addLink(v);
-            });
-            this.headings = headings;
+    define("require", ["exports"], function (exports) {
+        Object.defineProperty(exports, "__cjsModule", { value: true });
+        Object.defineProperty(exports, "default", { value: function (name) { return resolve(name); } });
+    });
+    /**
+     * はてなブログの見出し要素にコピー用のリンクを設置、
+     * リンクをクリックすると、その見出しの応じたURLがコピーされる
+     */
+    define("headingLink", ["require", "exports"], function (require, exports) {
+        "use strict";
+        exports.__esModule = true;
+        exports.headingLinkCopy = void 0;
+        var ENTRY_SELECTOR = ".entry-content";
+        var HEADING_SELECTOR = ".entry-content > h1, .entry-content > h2, .entry-content > h3, .entry-content > h4, .entry-content > h5, .entry-content > h6";
+        var HEADING_CLASS_NAME = "heading";
+        function isElement(target) {
+            return target instanceof Element;
         }
-        /**
-         * 記事内のすべての見出し要素を取得する
-         */
-        searchAllHeading() {
-            return document.querySelectorAll(HEADING_SELECTOR);
-        }
-        /**
-         * 現在のページのURLを取得する
-         */
-        getEntryURL() {
-            return new URL(location.href);
-        }
-        /**
-         * targetの要素にクラス名を追加してリンク化する
-         */
-        addLink(target) {
-            target.classList.add(HEADING_CLASS_NAME);
-            target.style.cssText = "cursor: pointer";
-        }
-        /**
-         * クリックを監視して、見出しをクリックした場合
-         */
-        observe() {
-            const entryElement = document.querySelector(ENTRY_SELECTOR);
-            if (!entryElement) {
-                return;
+        var HeadingLinkCopy = /** @class */ (function () {
+            function HeadingLinkCopy() {
+                var _this = this;
+                var headings = this.searchAllHeading();
+                headings.forEach(function (v) {
+                    _this.addLink(v);
+                });
+                this.headings = headings;
             }
-            entryElement.addEventListener("click", (e) => {
-                const target = e.target;
-                if (!target) {
+            /**
+             * 記事内のすべての見出し要素を取得する
+             */
+            HeadingLinkCopy.prototype.searchAllHeading = function () {
+                return document.querySelectorAll(HEADING_SELECTOR);
+            };
+            /**
+             * 現在のページのURLを取得する
+             */
+            HeadingLinkCopy.prototype.getEntryURL = function () {
+                return new URL(location.href);
+            };
+            /**
+             * targetの要素にクラス名を追加してリンク化する
+             */
+            HeadingLinkCopy.prototype.addLink = function (target) {
+                target.classList.add(HEADING_CLASS_NAME);
+                target.style.cssText = "cursor: pointer";
+            };
+            /**
+             * クリックを監視して、見出しをクリックした場合
+             */
+            HeadingLinkCopy.prototype.observe = function () {
+                var _this = this;
+                var entryElement = document.querySelector(ENTRY_SELECTOR);
+                if (!entryElement) {
                     return;
                 }
-                if (!isElement(target)) {
-                    return;
+                entryElement.addEventListener("click", function (e) {
+                    var target = e.target;
+                    if (!target) {
+                        return;
+                    }
+                    if (!isElement(target)) {
+                        return;
+                    }
+                    if (target.classList.contains(HEADING_CLASS_NAME)) {
+                        window.alert(_this.getEntryURL());
+                    }
+                });
+            };
+            return HeadingLinkCopy;
+        }());
+        function headingLinkCopy() {
+            var h = new HeadingLinkCopy();
+            h.observe();
+        }
+        exports.headingLinkCopy = headingLinkCopy;
+    });
+    define("index", ["require", "exports", "headingLink"], function (require, exports, headingLink_1) {
+        "use strict";
+        exports.__esModule = true;
+        function main() {
+            (0, headingLink_1.headingLinkCopy)();
+        }
+        main();
+    });
+    
+    'marker:resolver';
+
+    function get_define(name) {
+        if (defines[name]) {
+            return defines[name];
+        }
+        else if (defines[name + '/index']) {
+            return defines[name + '/index'];
+        }
+        else {
+            var dependencies = ['exports'];
+            var factory = function (exports) {
+                try {
+                    Object.defineProperty(exports, "__cjsModule", { value: true });
+                    Object.defineProperty(exports, "default", { value: require(name) });
                 }
-                if (target.classList.contains(HEADING_CLASS_NAME)) {
-                    window.alert(this.getEntryURL());
+                catch (_a) {
+                    throw Error(['module "', name, '" not found.'].join(''));
                 }
-            });
+            };
+            return { dependencies: dependencies, factory: factory };
         }
     }
-    function headingLinkCopy() {
-        const h = new HeadingLinkCopy();
-        h.observe();
+    var instances = {};
+    function resolve(name) {
+        if (instances[name]) {
+            return instances[name];
+        }
+        if (name === 'exports') {
+            return {};
+        }
+        var define = get_define(name);
+        if (typeof define.factory !== 'function') {
+            return define.factory;
+        }
+        instances[name] = {};
+        var dependencies = define.dependencies.map(function (name) { return resolve(name); });
+        define.factory.apply(define, dependencies);
+        var exports = dependencies[define.dependencies.indexOf('exports')];
+        instances[name] = (exports['__cjsModule']) ? exports["default"] : exports;
+        return instances[name];
     }
-    exports.headingLinkCopy = headingLinkCopy;
-});
-define("src/index", ["require", "exports", "src/headingLink"], function (require, exports, headingLink_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function main() {
-        (0, headingLink_1.headingLinkCopy)();
+    if (entry[0] !== null) {
+        return resolve(entry[0]);
     }
-    main();
-});
+})();
